30450064e949aae3e4eec4faca71e87a674d5be7
lujjjh <i@codehut.me>
2016-01-27 18:03:14 +0800

    Add tests:load command

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import yargs from 'yargs'
 
 import Students from './commands/Students'
+import Tests from './commands/Tests'
 
 yargs.usage('Usage: ea-utils <command> [options]')
   .command('students:load', 'List students from the given Excel file (group by curriculum)', Students.load)
   .command('students:list', 'Get student list based on the result of students:load command (without duplication)', Students.list)
+  .command('tests:load', 'List tests from the given Excel file (group by test number)', Tests.load)
   .demand(1)
   .help('h')
   .alias('h', 'help')
